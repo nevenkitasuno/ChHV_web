ef9270471fe9f9d096e7aee971bfddd74c287040
nevenkitasuno <foo@bar.com>
2024-04-19 16:20:27 +0300

    publish to pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://nevenkitasuno.github.io",
+  base: "/ChHV_web",
   integrations: [tailwind()]
 });
